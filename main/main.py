77c029f96d9065ac954ffba7c8ea6194ccb3f967
Ghaith Dmirieh <60393141+GhaithDmirieh@users.noreply.github.com>
2020-08-11 10:57:01 +0200

    import used classes/modules

--- main/main.py
+++ main/main.py
@@ -1,3 +1,8 @@
-from model.basilisk import *
+import turtle
+import time
+import random
+from model.basilisk import Basilisk
+from model.object import Object
+from model.gamefield import Gamefield
 
 if __name__ == "__main__":
