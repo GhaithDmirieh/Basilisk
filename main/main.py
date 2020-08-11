14fbb628d2d385333495f1523b7c7c96383c42bd
Ghaith Dmirieh <60393141+GhaithDmirieh@users.noreply.github.com>
2020-08-11 11:02:53 +0200

    generate Objects

--- main/main.py
+++ main/main.py
@@ -5,4 +5,7 @@ from model.basilisk import Basilisk
 from model.object import Object
 from model.gamefield import Gamefield
 
-if __name__ == "__main__":
+basilisk = Basilisk("square", "red")
+apple = Object("square", "white")
+myGameField = Gamefield("test", "green")
+
