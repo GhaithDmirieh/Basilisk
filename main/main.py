c182d2490de1e446a99b87363667310216280eeb
Ghaith Dmirieh <60393141+GhaithDmirieh@users.noreply.github.com>
2020-08-11 11:04:48 +0200

    add funktion gameOver

--- main/main.py
+++ main/main.py
@@ -9,3 +9,20 @@ basilisk = Basilisk("square", "red")
 apple = Object("square", "white")
 myGameField = Gamefield("test", "green")
 
+def gameOver():
+    for item in basilisk.body:
+        if item.distance(basilisk.mouth) < 20:
+            basilisk.headline.clear()
+            basilisk.headline.write("Game Over", align= "center", font=("Courier", 18 ,"normal"))
+            time.sleep(1)
+            basilisk.mouth.goto(0,0)
+            basilisk.mouth.direction = "stop"
+        
+            for x in basilisk.body:
+                x.goto(1000, 1000)
+                basilisk.body.clear()
+                basilisk.score = 0
+                basilisk.speed = 0.1
+                basilisk.headline.clear()
+                basilisk.headline.write("Score:{} High Score: {}".format(basilisk.score, basilisk.high_score), align ="center", font=("Courier", 18 ,"normal"))
+
