a7748ba229c92d91b38fbed1581d6b2fb33e3262
Ghaith Dmirieh <60393141+GhaithDmirieh@users.noreply.github.com>
2020-08-14 20:20:07 +0200

    create new headline
    use it to show best list then save this list in text file after the game ends

--- main.py
+++ main.py
@@ -11,12 +11,30 @@ from model.object import Object
 from model.gamefield import Gamefield
 from model.headline import Headline
 
+highScoreList = [0,0,0,0,0]
+
+#try:
+#    f = open("text.txt", "r")
+#    fileContentInString = f.read()
+#    tempList =  [item for item in fileContentInString.split(',')]
+#    tempList.pop()
+#
+#    for i in range(0, len(tempList) -1):
+#        tempList[i] = tempList[i].strip(',')
+#        int(tempList[i])
+#        highScoreList[i] = tempList[i]
+#    f.close()
+#except:
+#    highScoreList = [0,0,0,0,0] # temp
+
 rootWindow = tk.Tk()
 subWindowForGamefiled = tk.Canvas(rootWindow, width=600, height=600)
 subWindowForBestList = tk.Canvas(rootWindow, width=200, height=570)
 subWindowForGamefiled.pack(side = tk.LEFT)
 subWindowForBestList.pack(side = tk.TOP)
 
+headlineContent = "Score: 0 High Score: 0"
+headlineForBestListContent = "Best List:"
 
 myGameField = Gamefield(subWindowForGamefiled, "green")
 bestListField = Gamefield(subWindowForBestList, "springgreen4")
@@ -36,14 +54,33 @@ for image in imageList:
 basilisk = Basilisk(myGameField.getRootWindow(), gifUp)
 apple = Object(myGameField.getRootWindow(), gifApple)
 
+headlineForGame = Headline(myGameField.getRootWindow(), headlineContent, 0, 250)
+headlineForBestList = Headline(bestListField.getRootWindow(), headlineForBestListContent, 0, 0)
+
 def gameOver():
-    myHeadline.writeHeadlineForGameOver()
+    headlineForGame.writeHeadlineForGameOver()
     time.sleep(1)
     basilisk.basiliskGoHome()
     basilisk.basiliskDeleteBody()
     basilisk.setScore(0)
     basilisk.setSpeed(0.1)
-    myHeadline.writeNewHeadline(basilisk.getScore(), basilisk.getHighScore())
+    headlineForGame.writeNewHeadline(basilisk.getScore(), basilisk.getHighScore())
+    saveHighScoreInFile()
+
+def saveHighScoreInFile():
+        if basilisk.getHighScore() in highScoreList:
+            return
+        else: # Erweitere die Liste mit einem Element dann sortiere sie und lösche das Element mit dem niedrigsten Wert
+            highScoreList.append(basilisk.getHighScore())
+            sorted(highScoreList)
+            if len(highScoreList) == 6:
+                del(highScoreList[0])
+                temp = "Best List:\n {} --> {}\n {} --> {}\n {} --> {}\n {} --> {}\n {} --> {}".format(1, highScoreList[4], 2, highScoreList[3], 3, highScoreList[2], 4, highScoreList[1], 5, highScoreList[0])
+                headlineForBestList.writeNewHeadlineForBestList(temp)
+                fileR = open("text.txt", "w")
+                for y in highScoreList:
+                    fileR.write("{},".format(str(y)))
+                fileR.close()
 
 if __name__ == "__main__":
 
