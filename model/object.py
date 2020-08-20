2056a127db6c96550d16a09e04426ee560642365
Nydis <59804636+Nydis@users.noreply.github.com>
2020-08-20 15:07:53 +0200

    Update object.py

--- model/object.py
+++ model/object.py
@@ -1,6 +1,7 @@
 import turtle
 import random
 from turtle import RawTurtle, TurtleScreen
+import shelve
 
 class Object:
     def __init__(self, window, shape):
@@ -12,12 +13,20 @@ class Object:
 
     def getObj(self):
         return self.obj
+    
+    def getYPos(self):
+        return self.obj.ycor()
+
+    def getXPos(self):
+        return self.obj.xcor()
 
     def setPos(self, x, y):
         self.obj.goto(x,y)
+        self.obj.sety(self.obj.ycor()-(self.obj.ycor()%20))
+        self.obj.setx(self.obj.xcor()-(self.obj.xcor()%20))
         return
     
     def randomPos(self): #Pos darf nicht in Snakes Körper oder Headline stehen
-        x = random.randint(-290,290)
-        y = random.randint(-290,290)
+        x = random.randint(-280,280)
+        y = random.randint(-280,280)
         self.setPos(x, y)
